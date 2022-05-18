6f53f4642851e92f75d4b2aae947d89c03a54d92
mohit pipaliya <pipaliyamohit4g@gmail.com>
2022-05-19 01:07:50 +0530

    first change

--- helloLife.py
+++ helloLife.py
@@ -1,51 +1,5 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "6048617e",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "hello life<3\n"
-     ]
-    }
-   ],
-   "source": [
-    "print(\"hello life<3\")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "4859d6b9",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+ans=input("are you hungry??")
+if ans=="yes":
+    print("let's eat bhajiya")
+else:
+    print("ok then goto toilet")
